9d419b6e5cff9772f64fe8b70b5bd06e201d354a
venkatbommidi23 <128401434+venkatbommidi23@users.noreply.github.com>
2026-01-03 16:50:51 +0530

    Add insertion sort implementation
    Implement normal insertion sort algorithm.

---
+++ insertion_sort.py
@@ -0,0 +1,33 @@
+#https://www.geeksforgeeks.org/problems/insertion-sort/1
+
+class Solution:
+    def insertionSort(self, arr,n=None):
+        
+        #Recursive Insertion Sort
+        
+        # if n is None:
+        #     n=len(arr)
+        # if n<=1:
+        #     return
+        # self.insertionSort(arr,n-1)
+        
+        # key=arr[n-1]
+        # j=n-2
+        # while(j>=0 and arr[j]>key):
+        #     arr[j+1]=arr[j]
+        #     j-=1
+        # arr[j+1]=key
+        
+        
+        #NORMAL INSERTION SORT
+        
+        for i in range(1, len(arr)):      # start from the 2nd element (index 1)
+            key = arr[i]                  # current element to be inserted
+            j = i - 1                     # start comparing with the previous elements
+            
+            while j >= 0 and arr[j] > key:  # shift elements greater than key to the right
+                arr[j + 1] = arr[j]
+                j = j - 1                  # move one step left
+                
+            arr[j + 1] = key              # place key in the correct sorted position
+
