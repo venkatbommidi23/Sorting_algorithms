4b3c06845b5aed2fbb85148fe1f6b7a1a53127b3
venkatbommidi23 <128401434+venkatbommidi23@users.noreply.github.com>
2026-01-03 16:55:34 +0530

    Add bubble sort implementation in Bubblesort.py
    Implement bubble sort algorithm in Python.

---
+++ Bubblesort.py
@@ -0,0 +1,46 @@
+#https://www.geeksforgeeks.org/problems/bubble-sort/1
+
+class Solution:
+    #Function to sort the array using bubble sort algorithm.
+    def bubbleSort(self,arr,last=None):
+        if last is None:
+            last=len(arr)-1
+        if last==0:   #Base Case when last is 0, it means the array is fully sorted
+            return  
+        
+        n=len(arr)
+        if n==1: #Edge Case if Array has one element
+            return
+        swapped=False
+        for j in range(last):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+                swapped=True
+        if not swapped:
+            return
+        self.bubbleSort(arr,last-1)
+        
+        
+        
+        #Iterative Method
+        
+        # for i in range(len(arr)):
+        #     swapped=False
+        #     for j in range(len(arr)-i-1):
+        #         if arr[j+1]<arr[j]:
+        #             arr[j],arr[j+1]=arr[j+1],arr[j]
+        #             swapped=True
+        #     if not swapped:
+        #         break
+        # return arr
+        
+        # Time Complexity
+        
+        # Best Case-O(n) - Array when Sorted
+        # Worst Case -O(n*2) 
+        
+        #Space Complexity- O(1)
+        
+        
+        
+        
